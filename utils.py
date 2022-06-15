358ee3c44f1e45259ab9251df1e5931a7edd4995
kostin <kostin>
2022-06-15 03:17:55 +0300

    created homework

---
+++ utils.py
@@ -0,0 +1,18 @@
+
+# с помощью функционального программирования (функций filter, map), итераторов/генераторов сконструировать запрос
+def build_query(it, cmd, value):
+    res = map(lambda v: v.strip(), it)
+    if cmd == 'filter':
+        res = filter(lambda v: value in v, res)
+    if cmd == 'sort':
+        value = bool(value)
+        res = sorted(res, reverse=value)
+    if cmd == 'unique':
+        res = set(res)
+    if cmd == 'limit':
+        value = int(value)
+        res = list(res)[:value]
+    if cmd == 'map':
+        value = int(value)
+        res = map(lambda v: v.split(' ')[value], res)
+    return res
